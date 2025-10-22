7ca25d57c9fd9a5a846e5d83a79c0c2500e77f94
laptop-msi <wlgns7922@gmail.com>
2025-10-22 12:28:23 +0900

    Add: 원점에서 변환 후 원래 좌표 복구

--- vertex_prac17.glsl
+++ vertex_prac17.glsl
@@ -2,18 +2,18 @@
 
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
-uniform mat4 translation;
-uniform mat4 rotation;
+uniform mat4 rotation, modify;	// rotation - 기본 회전, modify - 해당 면만 변환
 uniform bool useTranslation;
+uniform vec3 moveToOrigin;
 out vec3 vertexColor;
 
 void main()
 {
 	if (useTranslation) {
-		gl_Position = translation * rotation * vec4(position, 1.0);
+		gl_Position = rotation * modify * vec4(position - moveToOrigin, 1.0) + vec4(moveToOrigin, 0);
 	}
 	else {
-	gl_Position = rotation * vec4(position, 1.0);
+		gl_Position = rotation * vec4(position, 1.0);
 	}
 	vertexColor = color;
 } 
